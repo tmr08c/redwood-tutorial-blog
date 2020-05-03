7cf21df98428201b9c26ec16a9f9c421ecb518cc
Troy Rosenberg <tmr08c@gmail.com>
2020-05-03 17:18:59 -0400

    Add "user facing" list of blog posts.
    https://redwoodjs.com/tutorial/routing-paramshttps://redwoodjs.com/tutorial/routing-params

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,7 +1,15 @@
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const HomePage = () => {
-  return <BlogLayout>Home</BlogLayout>
+  return (
+    <BlogLayout>
+      <h1>Redwood Tutorial Blog</h1>
+      <h2>Posts</h2>
+      <BlogPostsCell />
+    </BlogLayout>
+  )
 }
 
 export default HomePage
