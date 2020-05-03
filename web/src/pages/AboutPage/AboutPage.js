3632836c3a70265f520ad7967fa57455c3b25814
Troy Rosenberg <tmr08c@gmail.com>
2020-05-03 12:26:00 -0400

    Creates BlogLayout
    Extracts a layout from the shared header code between the Home and About pages.

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,29 +1,16 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link } from '@redwoodjs/router'
+
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const AboutPage = () => {
   return (
-    <>
-      <header>
-        <h1>Redwood Tutorial Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          This site was created as a part of the{' '}
-          <Link to="https://redwoodjs.com/tutorial">Redwood Tutorial</Link>.
-        </p>
-        <p>So far, so good.</p>
-        <p>
-          Why don't you just go <Link to={routes.home()}>Home</Link>
-        </p>
-      </main>
-    </>
+    <BlogLayout>
+      <p>
+        This site was created as a part of the{' '}
+        <Link to="https://redwoodjs.com/tutorial">Redwood Tutorial</Link>.
+      </p>
+      <p>So far, so good.</p>
+    </BlogLayout>
   )
 }
 
